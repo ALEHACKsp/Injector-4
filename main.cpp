6985d29803433a0ba865d0ef6c852fc2f51905ed
Fiereu <32392522+Fiereu@users.noreply.github.com>
2020-06-06 22:48:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,8 +12,14 @@
 #define BUFSIZE 4096
 
 FILE* stream;
-char TARGET_PROCESS_NAME[BUFSIZE] = "ac_client.exe";
-char PAYLOAD_NAME[BUFSIZE] = "Assaultcube-hax.dll";
+char TARGET_PROCESS_NAME[BUFSIZE] = "PROCESS.exe";
+char PAYLOAD_NAME[BUFSIZE] = "YOUR.dll";
+
+void ConsoleSetup();
+int GetProcessIdByName(PCSTR name);
+int Inject(int pid);
+
+
 void ConsoleSetup() {
 	AllocConsole();
 	freopen_s(&stream, "CONOUT$", "w", stdout);
@@ -59,22 +65,14 @@ int Inject(int pid) {
 		return -1;
 	}
 	char PayloadPath[BUFSIZE];
-	/*GetCurrentDirectoryA(BUFSIZE, (LPSTR)PayloadPath);
-	std::string s(PayloadPath);
-	std::string d = "\\";
 	
-	std::string f(PAYLOAD_NAME);
-	s = s + d + f;
-	*/
 	
 	std::string s(PAYLOAD_NAME);
 	lstrcpyA(PayloadPath, s.c_str());
 	std::cout << "[+]PayloadPath " << PayloadPath << std::endl;
 	LPVOID RemotePayloadspace = VirtualAllocEx(hProcess, NULL, BUFSIZ, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 	std::cout << "[+]RemotePayloadspace 0x" << std::hex << RemotePayloadspace << std::endl;
-	/*HANDLE hpayload = CreateFile(c, GENERIC_READ, 0, NULL, 3, FILE_ATTRIBUTE_NORMAL, NULL);
-	LPVOID payloadbuf;
-	BOOL successread = ReadFile(hpayload,payloadbuf,);*/
+	
 	BOOL success = WriteProcessMemory(hProcess, RemotePayloadspace, (LPVOID)PayloadPath, BUFSIZ, NULL);
 	if (!success) {
 		std::cout << "[-]Can't write Processmemory!!!" << std::endl;
@@ -92,16 +90,6 @@ int Inject(int pid) {
 
 	return 0;
 }
-bool testfile(const std::string& name) {
-	FILE* stream;
-	if (fopen_s(&stream, name.c_str(), "r")) {
-		fclose(stream);
-		return true;
-	}
-	else {
-		return false;
-	}
-}
 
 int main() {
 	
@@ -117,13 +105,6 @@ int main() {
 		else {
 			std::cout << std::endl << "[+]Found Process '" << TARGET_PROCESS_NAME << "' with PID " << pid << std::endl;
 			Sleep(3000);
-			/*if (GeneratePayload() == 0) {
-				std::cout << "[+]Payload generated" << std::endl;
-			}
-			else {
-				std::cout << "[+]Error while generating payload" << std::endl;
-				goto EXIT;
-			}*/
 			if (Inject(pid) == 0) {
 				std::cout << "[+]Success :D" << std::endl;
 			}
